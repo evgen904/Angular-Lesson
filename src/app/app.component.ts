fc0b82e52b912b5f4c81d0d7d4c3c9c4988f7305
Evgen904 <evgen904@gmail.com>
2021-04-05 00:14:24 +0400

    Интерполяция

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
+
+  titleDynamic = 'titleDynamic';
+  numberText = 42;
+  arr = [1, 2 , 3 , 4, 5];
+
+  obj = {
+    a: 2,
+    b: {
+      c: 3
+    }
+  };
+
+
+
 }
