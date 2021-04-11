3fbf4777bf08e2ce3a7ac04f3ca113dcdf2d0ec2
Evgen904 <evgen904@gmail.com>
2021-04-12 00:47:19 +0400

    Циклы с ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,6 +31,24 @@ export class AppComponent {
 
   toggle: any = false
 
+  arr2 = [1,1,2,3,5,8,13]
+
+  objs = [
+    {post: 'Post 1', author: 'Evgeniy', comment: [
+        {name: 'Max 1', text: 'Lorem 1'},
+        {name: 'Max 2', text: 'Lorem 2'},
+        {name: 'Max 3', text: 'Lorem 3'}
+      ]},
+    {post: 'Post 2', author: 'Ksusha', comment: [
+        {name: 'Max 11', text: 'Lorem 11'},
+        {name: 'Max 22', text: 'Lorem 22'},
+        {name: 'Max 33', text: 'Lorem 33'}
+      ]}
+  ]
+
+
+
+
   constructor() {
     setTimeout(() => {
       this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
