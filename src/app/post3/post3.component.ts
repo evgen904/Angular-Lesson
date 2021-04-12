b43decbcb9368bf461ebbcd5e4a4c13082c3d970
Evgen904 <evgen904@gmail.com>
2021-04-13 00:00:50 +0400

    Как создавать компоненты

--- src/app/post3/post3.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-post3',
-  templateUrl: './post3.component.html',
-  styleUrls: ['./post3.component.scss']
-})
-export class Post3Component {}
