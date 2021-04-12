bb787f41e09b8cdb5d4f15616a861361f0477726
Evgen904 <evgen904@gmail.com>
2021-04-13 00:54:20 +0400

    Передача параметров

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    {title: 'Первый блок', text: 'Описание блока', id: 1},
+    {title: 'Второй блок блок', text: 'Описание для второго блока', id: 2}
+  ]
 }
