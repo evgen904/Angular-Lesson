127e57c9b76a0eebc355a10cdcc4835b2108e463
Evgen904 <evgen904@gmail.com>
2021-04-18 01:13:13 +0400

    Как работает HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -11,6 +11,8 @@ export class StyleDirective {
 
   @Input() dStyle!: {border?: string, borderRadius?: string}
 
+  @HostBinding('style.color') elColor = ''
+
   constructor(private el: ElementRef, private r: Renderer2) {
     //this.r.setStyle(this.el.nativeElement, 'color', '#c00')
   }
@@ -20,17 +22,19 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', this.color)
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
-    this.r.setStyle(this.el.nativeElement, 'border', this.dStyle.border)
-    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius)
+    this.elColor = this.color
+    // this.r.setStyle(this.el.nativeElement, 'color', this.color)
+    // this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+    // this.r.setStyle(this.el.nativeElement, 'border', this.dStyle.border)
+    // this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius)
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', null)
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
-    this.r.setStyle(this.el.nativeElement, 'border', null)
-    this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
+    this.elColor = ''
+    // this.r.setStyle(this.el.nativeElement, 'color', null)
+    // this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
+    // this.r.setStyle(this.el.nativeElement, 'border', null)
+    // this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
   }
 
 }
