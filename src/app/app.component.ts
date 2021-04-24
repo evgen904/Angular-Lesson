ee8d150af7f0c2f1906fdf8979d51d100426b07a
Evgen904 <evgen904@gmail.com>
2021-04-25 01:15:02 +0400

    Пайпы. Работа с числами

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,30 +7,11 @@ export interface Post {
 })
 export class AppComponent implements OnInit{
 
-  isVisible = true
-
-  posts: Post[] = [
-    {title: 'Первый блок', text: 'Описание блока', id: 1},
-    {title: 'Второй блок блок', text: 'Описание для второго блока', id: 2}
-  ]
+  e: number = Math.E
 
   ngOnInit(): void {
-    setTimeout(() => {
-      console.log('Timeout')
-      this.posts[0] = {
-        title: '123',
-        text: '123 12313',
-        id: 123
-      }
-    }, 5000)
+
   }
 
-  updatePost(post: Post) {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter(item => item.id !== id)
-  }
 
 }
