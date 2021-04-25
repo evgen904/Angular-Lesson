09a5da59b6644356267565b5daf8f5eaf57abccd
Evgen904 <evgen904@gmail.com>
2021-04-25 20:50:52 +0400

    Генерация пайпа с CLI

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+interface Post {
+  title: string
+  text: string
+}
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(posts: Post[], search: string = '', field: string = 'title'): Post[] {
+    if (!search.trim()) {
+      return posts
+    }
+
+    return posts.filter(post => {
+      // @ts-ignore
+      return post[field].toLowerCase().includes(search.toLowerCase())
+    })
+  }
+
+}
